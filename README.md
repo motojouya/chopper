73871cdeacaa07f72e6bc13f665a8c6cc931b297
motojouya <motojouya@gmail.com>
2019-02-27 22:15:41 +0900

    function works

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+
+node_modules/.bin/netlify-lambda serve src/functions
+
+
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ src/functions/mariegohan.js
@@ -0,0 +1,33 @@
+const launchChrome = require('@serverless-chrome/lambda')
+const CDP = require('chrome-remote-interface')
+const puppeteer = require('puppeteer')
+
+exports.handler = async (event, context, callback) => {
+  try {
+    const slsChrome = await launchChrome.default();
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: (await CDP.Version()).webSocketDebuggerUrl
+    });
+    const page = await browser.defaultBrowserContext().newPage();
+
+    await page.goto('https://mariegohan.com/12650', { waitUntil: 'domcontentloaded' });
+    const searchResults = await page.evaluate(() => {
+      const ret = [];
+      const titles = document.querySelectorAll("h1");
+      const title = titles[0].innerText;
+      console.log(title);
+
+      // nodeList.forEach(node => {
+      //   ret.push(node.innerText);
+      // });
+
+      return title;
+    });
+    return callback(null, {
+      statusCode: 200,
+      body: JSON.stringify({ searchResults: searchResults })
+    });
+  } catch (err) {
+    return callback(err);
+  }
+}
